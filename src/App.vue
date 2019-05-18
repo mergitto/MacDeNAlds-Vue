048a826ca9c6f1d3ba6c9280bba26e4634919faf
mergitto <merugitosu@gmail.ccom>
2019-05-18 18:39:14 +0900

    add grid design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
+  <div class="container">
+    <header>
+      <h1>{{ msg }}</h1>
+    </header>
+    <div id="app">
+      <div id="customer-col">
+      </div>
+      <div id="register-col">
+      </div>
+      <div id="food-col">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,17 +26,37 @@ export default {
 </script>
 
 <style lang="scss">
+header {
+  margin: 40px;
+}
+h1 {
+  margin: auto;
+  font-weight: normal;
+  text-align: center;
+}
+.container {
+  text-align: center;
+  width: 100vw;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  height: 70vh;
 }
-
-h1 {
-  font-weight: normal;
+#customer-col {
+  background-color: aquamarine;
+  width: 50vw;
 }
-
+#register-col {
+  background-color: darkgoldenrod;
+  width: 25vw;
+}
+#food-col {
+  background-color: chocolate;
+  width: 25vw;
+}
 </style>
